a4b615680a6aab021754c1e7be6559ea977b4f47
jjy0918 <raid1597@naver.com>
2021-01-20 14:00:50 +0900

    Update 트라이.cpp
    트라이 char* 사용 방법 추가

--- Algorithm 사용 코드/자료구조/트라이.cpp
+++ Algorithm 사용 코드/자료구조/트라이.cpp
@@ -26,10 +26,12 @@ struct Trie
 				delete childNode[i];
 		}
 	}
-
+	
+	// void insert(char *c)
 	void insert(string& str, int i)
 	{
-    	// 문자열 끝인 경우
+    		// 문자열 끝인 경우
+		// str.size()==i  =>  *c=='\0'
 		if (str.size() == i)
 		{
 			isFinish = true;
@@ -37,30 +39,37 @@ struct Trie
 		}
 		else
 		{
+			// nextNode = *c-'A';
 			int nextNode = str[i] - 'A';
+			
 			if (childNode[nextNode] == NULL)
 			{
 				childNode[nextNode] = new Trie();
 			}
+			
+			//insert(c+1);
 			childNode[nextNode]->insert(str, i + 1);
 		}
 	}
-
+	
+	// bool find(char *c)
 	bool find(string& str,int i)
 	{
 		// 해당 문자가 기존의 트라이에 있는지 판단.
 		// abc라는 문자만 트라이에 있고, ab를 탐색할 때 해당 노드는 
 		// 트라이에 없기 때문에 isFinish(false) 출력
+		// str.size()==i  =>  *c=='\0'
 		if (str.size() == i)
 		{
 			return isFinish;
 		}
 		
+		// nextNode = *c-'A';
 		int nextNode = str[i] - 'A';
 
 		if (childNode[nextNode] == NULL)
 			return false;
-
+		//find(c+1);
 		return childNode[nextNode]->find(str,i+1);
 	}
 };
